5a5ebebf0f815941945e2ae9076986aa9dcafaec
CandiceSG <candice.salagasperini@gmail.com>
2014-10-07 17:55:56 +0200

    exo 5

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,11 @@
 require_relative "coach_answer"
-# TODO: Implement the program that makes you discuss with your coach from the terminal.
+# TODO: Implement the program that makes you discuss with your coach from the terminal.P
+puts "What do you want to tell to your coach ?"
+your_message=gets.chomp
+puts coach_answer(your_message)
+  puts "So what ?"
+
+while (your_message != "I am going to work right now!".upcase) || (your_message != "I am going to work right now!")
+  your_message = gets.chomp
+  puts coach_answer_enhanced(your_message)
+end
