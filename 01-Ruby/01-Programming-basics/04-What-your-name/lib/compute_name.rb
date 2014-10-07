13d8d6d8b090161e47ae96a5f1ef3203cd2bb7e9
CandiceSG <candice.salagasperini@gmail.com>
2014-10-07 15:30:10 +0200

    exo 4

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/compute_name.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/compute_name.rb
@@ -1,3 +1,4 @@
 def compute_name(first_name, middle_name, last_name)
-  # TODO: return full name using string interpolation
+  name = "#{first_name} #{middle_name} #{last_name}"
+ return name # TODO: return full name using string interpolation
 end
