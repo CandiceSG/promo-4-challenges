db0bf3aed53015cb7e7fb40e9300d7d7afb29180
CandiceSG <candice.salagasperini@gmail.com>
2014-10-09 00:49:26 +0200

    exo 4

--- 01-Ruby/02-Arrays-Enumerable/04-Debug-Gym/lib/bugged_concatenate.rb
+++ 01-Ruby/02-Arrays-Enumerable/04-Debug-Gym/lib/bugged_concatenate.rb
@@ -1,8 +1,7 @@
-def bugged_concatenate(i, f, b, c, d)
-  array = []
+def bugged_concatenate(array)
   array.reduce("") { |a, e| a + e }.upcase
 end
 
 def build_1984_title
-  bugged_concatenate(1, "9", 84, " ", "George Orwell").to_s
+  bugged_concatenate(["1", "9", "84", " ", "George Orwell"])
 end
