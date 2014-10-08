80bcf9de0c9ddc8722d0897c6aaab62772f1c5d8
CandiceSG <candice.salagasperini@gmail.com>
2014-10-08 22:02:34 +0200

    exo 3 quasi fini

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -1,29 +1,27 @@
 require_relative 'black_jack'
 
 def state_of_the_game(player_score, bank_score)
-    player_score << pick_player_card
-return "The bank score is #{pick_bank_score.to_s} and the the player score is #{player_score.to_s}"
+  "The bank score is #{bank_score} and the player score is #{player_score}"
 end
 
 def end_game_message(player_score, bank_score)
- if player_score == 21
-   "Black Jack! You win"
+  if player_score == 21
+    "Black Jack! You win"
 elsif player_score > 21
-  "You lose !"
+    "You lose !"
 elsif player_score > bank_score && player_score < 21
-  "You win"
+    "You win"
 else
   "You lose!"
-end
-  # TODO: return (not print!) a message telling if the user won or lost.
+  end
 end
 
 
 def play_game
-player_score = 0
-while answer == "yes" || answer == "y"
-  player_score += pick_player_card
-  puts "Card ? (type 'y' or 'yes' for a new card)"
-end
+  player_score = 0
+  while answer == "yes" || answer == "y"
+    player_score += pick_player_card
+    puts "Card ? (type 'y' or 'yes' for a new card)"
+  end
 end
 
