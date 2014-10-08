37d3fa0106151109d4f5defc8ac680aaa445f5a4
CandiceSG <candice.salagasperini@gmail.com>
2014-10-08 18:49:53 +0200

    exo 3 à finir

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
@@ -1,8 +1,16 @@
+player_score = 0
+bank_score = [16..21]
+p_card = [1, 11]
+
+
+
 def pick_bank_score
+ return bank_score.sample
   # TODO: Use Random to get a new random score
 end
 
-def pick_card
+def pick_player_card
+ return p_card.sample
   # TODO: Use Random to get a new random card
 end
 
