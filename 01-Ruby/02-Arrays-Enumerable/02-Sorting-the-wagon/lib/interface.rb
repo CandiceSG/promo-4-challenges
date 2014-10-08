4ea7739663d66ef165176b9f7b27c92962a402e8
CandiceSG <candice.salagasperini@gmail.com>
2014-10-08 16:59:21 +0200

    exo 2

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -1,11 +1,26 @@
 require_relative "wagon_sort"
 
-# TODO: Ask the user about students to add to the Wagon.
-#       Remember, to read an input from the command line, use:
-#       - `gets`:  http://www.ruby-doc.org/core-2.1.2/Kernel.html#method-i-gets
-#       - `chomp`: http://www.ruby-doc.org/core-2.1.2/String.html#method-i-chomp
+student_array = []
 
 
+puts "Student name ?"
+answer = gets.chomp
+    student_array << answer
 
-# TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
-#       file and display the sorted student list
+while answer != ""
+    puts "Type another student name (or press enter to finish)"
+    answer = gets.chomp
+    if answer != ""
+      student_array << answer
+    end
+
+
+end
+
+student_array = wagon_sort(student_array)
+
+first = student_array[0..-2]
+last = student_array.last
+
+puts "Congratulations ! Your wagon has #{student_array.size} students :"
+puts "- #{first.join(", ")} and #{last}"
--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,4 @@
 def wagon_sort(students)
+ students.sort_by {|a| a.downcase }
   # TODO: return (not print!) a copy of students, sorted alphabetically
 end
