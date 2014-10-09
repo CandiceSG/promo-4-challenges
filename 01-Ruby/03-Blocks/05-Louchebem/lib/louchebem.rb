93086deb84fe92887f50ef64fc69d15929e66f09
CandiceSG <candice.salagasperini@gmail.com>
2014-10-09 18:56:59 +0200

    exo 5 quasi fini

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -1,15 +1,12 @@
-# Encoding: utf-8
-
-def louchebemize(word)
+def louchebemize_word(word)
   suffixe = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
   suffixe_choisi = suffixe.sample
-
   if word.size <= 1
     puts word
     return word
-  elsif word.downcase.start_with?('a','e','i','o','u','é', 'è','ê','y')
+  elsif word.downcase.start_with?('a', 'e', 'i', 'o', 'u', 'é', 'è', 'ê', 'y')
     word = "l" + word
-  elsif !word.downcase.start_with?('a','e','i','o','u','é', 'è','ê','y')
+  elsif !word.downcase.start_with?('a', 'e', 'i', 'o', 'u', 'é', 'è', 'ê', 'y')
     word += word[0]
     word[0] = ""
     return louchebemize(word)
@@ -20,4 +17,8 @@ def louchebemize(word)
 end
 
 
-louchebemize("fou")
+def louchebemize(sentence)
+  words = sentence.split(" ")
+  words.map! { | word | louchebemize_word(word) }
+ return words.join(" ")
+end
