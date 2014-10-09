fa63e562da17808ee107435c282d55d2d901dbd4
CandiceSG <candice.salagasperini@gmail.com>
2014-10-09 12:49:24 +0200

    exo timer fait

--- 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Blocks/02-About-blocks/lib/block_gym.rb
@@ -1,8 +1,13 @@
-def tag(tag_name, attr = nil)
-  # TODO:  Build HTML tags around  content given in the block
-  #        The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
-end
 
 def timer_for
-  # TODO:  Return time taken to execute the given block
+  start_time = Time.now
+  yield
+  duration = (Time.now - start_time).to_f
+  return duration
+end
+
+
+timer_for do
+  puts "hello"
+  sleep(4)
 end
