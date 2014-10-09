b7f707b9e11685c91e3f40c882300bb83836fe23
CandiceSG <candice.salagasperini@gmail.com>
2014-10-09 17:59:34 +0200

    exo 5 quasi fini

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -1,5 +1,23 @@
 # Encoding: utf-8
 
-def louchebemize(sentence)
-  # TODO: implement your louchebem translator
+def louchebemize(word)
+  suffixe = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
+  suffixe_choisi = suffixe.sample
+
+  if word.size <= 1
+    puts word
+    return word
+  elsif word.downcase.start_with?('a','e','i','o','u','é', 'è','ê','y')
+    word = "l" + word
+  elsif !word.downcase.start_with?('a','e','i','o','u','é', 'è','ê','y')
+    word += word[0]
+    word[0] = ""
+    return louchebemize(word)
+  end
+  puts word
+  puts word + suffixe_choisi
+  word + suffixe_choisi
 end
+
+
+louchebemize("fou")
