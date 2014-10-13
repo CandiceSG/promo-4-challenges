816020cd80937281c418bd4caf6c2d2d9c2c114c
CandiceSG <candice.salagasperini@gmail.com>
2014-10-13 15:34:02 +0200

    exo 1 fini

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,8 @@
 def french_phone_number?(phone_number)
-  # TODO: true or false?
+  phone_number = phone_number.gsub(/\s|-/, "")
+  if phone_number =~ /^(\+33|0)[1-9]\d{8}/
+    return true
+  else
+    return false # TODO: true or false?
+  end
 end
