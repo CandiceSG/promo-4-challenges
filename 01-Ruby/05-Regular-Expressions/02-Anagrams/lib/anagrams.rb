5ac9f91e17bd65f24e6410abfe6be3152c150c2b
CandiceSG <candice.salagasperini@gmail.com>
2014-10-14 12:56:55 +0200

    exo1

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,5 +1,5 @@
 def anagrams?(a_string, another_string)
-  # TODO: implement the obvious method to test if two words are anagrams
+ a_string = another_string.sort # TODO: implement the obvious method to test if two words are anagrams
 end
 
 def anagrams_on_steroids?(a_string, another_string)
--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -1,6 +1,32 @@
 # Encoding: utf-8
 require 'csv'
 
+
 def most_successfull(number, max_year, file_name)
-  # TODO: return the number most successfull movies max_year
+  movies = []
+  csv_options = { col_sep: ',' }
+
+  CSV.foreach(file_name, csv_options) do |row|
+    movies << {
+      name: row[0],
+      year: row[1].to_i,
+      earnings: row[2].to_i
+    }
+  end
+
+  sorted_movies = movies.sort_by do |movie|
+    - movie[:earnings]
+  end
+
+  movies_max_year = sorted_movies.select do |movie|
+    movie[:year] < max_year
+  end
+
+  return movies_max_year.take(number)
 end
+
+  filepath = "/Users/candicesala/code/CandiceSG/promo-4-challenges/01-Ruby/06-Parsing/01-CSV-Parsing/lib/movies.csv"
+
+p most_successfull(8, 1975, filepath)
+
+# TODO: return the number most successfull movies max_year
