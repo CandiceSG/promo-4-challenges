6c98b91a418381c882c64562232acf49663d5b90
CandiceSG <candice.salagasperini@gmail.com>
2014-10-16 16:20:13 +0200

    exo2 never too late

--- 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,37 @@
 class Restaurant
   # TODO: add relevant accessors if necessary
 
+  attr_reader :name, :city, :average_rating
+
   def initialize(city, name)
+    @city = city
+    @name = name
+    @average_rating = 0
+    @rates = []
     # TODO: implement constructor with relevant instance variables
   end
 
-  # TODO: implement #filter_by_city and #rate methods
+  def rate(new_rate)
+    @rates << new_rate
+    @average_rating = @rates.inject(:+) / @rates.size
+  end
+
+  def self.filter_by_city(restaurants, city)
+    restaurants.select { |restaurant|  restaurant.city == city }
+  end
 end
+
+jules_verne = Restaurant.new("paris", "Jules Verne")
+tour_d_argent = Restaurant.new("paris", "Tour d'argent")
+bocuse = Restaurant.new("lyon", "Paul Bocuse")
+restos = [jules_verne, tour_d_argent, bocuse]
+Restaurant.filter_by_city(restos, "lyon")
+
+
+# filtered_restaurant = []
+# restaurants.each do |restaurant|
+# if restaurant.city == city
+# filtered_restaurant << restaurant
+# end
+# end
+# TODO : implement #filter_by_city and #rate methods
