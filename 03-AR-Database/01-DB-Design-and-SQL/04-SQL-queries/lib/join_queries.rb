e992fc6d51a9c0bf22ce24372b192a70e9c68380
CandiceSG <candice.salagasperini@gmail.com>
2014-10-23 17:49:27 +0200

    exo 4

--- 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
@@ -1,11 +1,15 @@
 require 'sqlite3'
 
 def detailed_tracks(db)
- array = db.execute"SELECT t.name, alb.title, art.name FROM tracks t JOIN albums alb ON alb.id = t.album_id JOIN artists art ON art.id = alb.artist_id"
+  array = db.execute "SELECT t.name, alb.title, art.name
+  FROM tracks t JOIN albums alb ON alb.id = t.album_id
+  JOIN artists art ON art.id = alb.artist_id"
   return array
 end
 
 def stats_on(db, genre_name)
+  hash = {}
+
   # TODO: For the given category of music, return the number of tracks and the average song length (as a stats hash)
 end
 
