3b0b50c312bac05721cbc76f0e86c47a786b16b3
CandiceSG <candice.salagasperini@gmail.com>
2014-10-23 15:53:17 +0200

    ok

--- 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
@@ -7,17 +7,16 @@ def number_of_rows(db, table_name)
 end
 
 def sorted_artists(db)
- array = db.execute "SELECT name FROM artists ORDER BY name ASC"
- p array.flatten # TODO: return array of artists' names sorted alphabetically
+  array = db.execute "SELECT name FROM artists ORDER BY name ASC"
+  p array.flatten # TODO: return array of artists' names sorted alphabetically
 end
 
 def love_tracks(db)
-array = db.execute "SELECT name FROM tracks WHERE name LIKE '%love%'" # TODO: return array of love songs
-p array.flatten
+  array = db.execute "SELECT name FROM tracks WHERE name LIKE '%love%'" # TODO: return array of love songs
+  p array.flatten
 end
 
 def long_tracks(db, min_length)
- array = db.execute "SELECT name FROM tracks WHERE milliseconds > #{min_length * 60000} ORDER BY milliseconds ASC" # TODO: return tracks verifying: duration > min_length (minutes)
-p array.flatten
-
+  array = db.execute "SELECT name FROM tracks WHERE milliseconds > #{min_length * 60_000} ORDER BY milliseconds ASC"
+  p array.flatten
 end
