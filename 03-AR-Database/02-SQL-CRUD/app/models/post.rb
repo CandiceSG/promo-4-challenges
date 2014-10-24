3e05d5e861f96c6c52129882f01d4f857b4cb4f6
CandiceSG <candice.salagasperini@gmail.com>
2014-10-24 16:56:16 +0200

    exo 1

--- 03-AR-Database/02-SQL-CRUD/app/models/post.rb
+++ 03-AR-Database/02-SQL-CRUD/app/models/post.rb
@@ -13,8 +13,8 @@ class Post
     @id = options[:id]
     @title = options[:title]
     @url = options[:url]
-    @date = Time.now
-    @votes = 0 || options[:votes]
+    @date = options[:date] || Time.now
+    @votes = options[:votes] || 0
   end
 
   def upvote
@@ -36,19 +36,37 @@ class Post
     DB.execute("DELETE FROM posts WHERE id = '#{@id}';")
   end
 
+  # me renvoie un tableau de tableau donc je lui demande le premier tableau avec l'index 0
   def self.find(id)
     columns = DB.execute("SELECT * FROM posts WHERE id = #{id};")[0]
-    # me renvoie un tableau de tableau donc je lui demande le premier tableau avec l'index 0
     if columns.nil?
       nil
     else
-      new_post = Post.new(title: columns[1], url: columns[2], date: Time.at(columns[3]), vote: columns[4], id: columns[0])
+      new_post = Post.new(
+        title: columns[1],
+        url: columns[2],
+        date: Time.at(columns[3]),
+        vote: columns[4],
+        id: columns[0]
+        )
     end
     return new_post
   end
 
 
   def self.all
+    columns = DB.execute("SELECT * FROM posts;")
+    posts = []
+    columns.each do |post|
+      posts << Post.new(
+        id: post[0],
+        title: post[1],
+        url: post[2],
+        date: Time.at(post[3]),
+        vote: post[4]
+        )
+    end
+    posts
   end
 
 
@@ -61,10 +79,11 @@ class Post
 end
 
 # post = Post.new({ title: "Le Wagon", url: "http://www.lewagon.org" })
-# post.save
+# post2 = Post.new({title: "google", url: "http://google.com"})
+# # post.save
 
 
-# p Post.find(0)
+# p Post.all
 
 
 
