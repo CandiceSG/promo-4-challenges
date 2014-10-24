6ea53f06d31df00c044649d31647b1a060456c40
CandiceSG <candice.salagasperini@gmail.com>
2014-10-24 16:12:26 +0200

    exo 1

--- 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/04-SQL-queries/lib/join_queries.rb
@@ -8,10 +8,13 @@ def detailed_tracks(db)
 end
 
 def stats_on(db, genre_name)
-  hash = {}
-  # TODO: For the given category of music, return the number of tracks and the average song length (as a stats hash)
+hash = {}
 end
 
+# For each genre of music, find the stats,
+# i.e. the number of tracks and the average song length (in minutes).
+# Tip: you have to use a GROUP BY statement with aggregate SQL functions such as COUNT and AVG.
+# The method should return a Hash of statistics, for example:
 def top_five_artists(db, genre_name)
  array = [] # TODO: return list of top 5 rock artists
 end
--- 03-AR-Database/02-SQL-CRUD/app/models/post.rb
+++ 03-AR-Database/02-SQL-CRUD/app/models/post.rb
@@ -1,13 +1,82 @@
 # You can use a global variable, DB, built as is:
-# DB = SQLite3::Database.new(file)
+require "sqlite3"
+
+dir = File.dirname(__FILE__)
+DB = SQLite3::Database.new(File.join(dir, "../../db/posts.db"))
+
 
 class Post
-  # TODO: implement some reader and/or writers
+  attr_reader :id, :date, :votes # TODO: implement some reader and/or writers
+  attr_accessor :title, :url
 
   def initialize(options = {})
-    # TODO: initialize instance variables from the hash options.
+    @id = options[:id]
+    @title = options[:title]
+    @url = options[:url]
+    @date = Time.now
+    @votes = 0 || options[:votes]
   end
+
+  def upvote
+    @votes += 1
+  end
+
+  def save
+    if @id.nil?
+      DB.execute("INSERT INTO posts (title, url, date, votes)
+                VALUES ('#{@title}', '#{@url}', '#{@date.to_i}', #{@votes});")
+      @id = DB.last_insert_row_id
+    else
+      DB.execute("UPDATE posts
+            SET title = '#{@title}', url = '#{@url}', date = '#{@date.to_i}', votes = #{@votes} WHERE id = #{@id};")
+    end
+  end
+
+  def destroy
+    DB.execute("DELETE FROM posts WHERE id = '#{@id}';")
+  end
+
+  def self.find(id)
+    columns = DB.execute("SELECT * FROM posts WHERE id = #{id};")[0]
+    # me renvoie un tableau de tableau donc je lui demande le premier tableau avec l'index 0
+    if columns.nil?
+      nil
+    else
+      new_post = Post.new(title: columns[1], url: columns[2], date: Time.at(columns[3]), vote: columns[4], id: columns[0])
+    end
+    return new_post
+  end
+
+
+  def self.all
+  end
+
+
+
+  # TODO: initialize instance variables from the hash options.
+  # end
 
   # TODO: implement all the class methods and instance methods
   #       to have a full CRUD support on your Post model
 end
+
+# post = Post.new({ title: "Le Wagon", url: "http://www.lewagon.org" })
+# post.save
+
+
+# p Post.find(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
