dff9a9f88f0cfc3f96b5db085e72bbe71e3365b2
CandiceSG <candice.salagasperini@gmail.com>
2014-10-27 18:11:43 +0100

    exo 4

--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/db/seeds.rb
@@ -6,9 +6,9 @@ response = RestClient.get "https://hacker-news.firebaseio.com/v0/topstories.json
 repos = JSON.parse(response).take(10)
 
 repos.each do |id|
-url = "https://hacker-news.firebaseio.com/v0/item/#{id}.json"
-  Json_post = RestClient.get(url)
-  post = JSON.parse(Json_post)
+  url = "https://hacker-news.firebaseio.com/v0/item/#{id}.json"
+  json_post = RestClient.get(url)
+  post = JSON.parse(json_post)
   Post.create(name: post["title"], url: post["url"], votes: post["score"])
 end
 
