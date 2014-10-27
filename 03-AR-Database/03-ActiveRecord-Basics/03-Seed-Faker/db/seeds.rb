262dbf0d8db7ea286059135c2191097bda23ae3a
CandiceSG <candice.salagasperini@gmail.com>
2014-10-27 16:48:50 +0100

    exo 3

--- 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
@@ -1,3 +1,10 @@
 require "faker"
 
+100.times do
+  post = Post.new({
+    name: Faker::Company.name,
+    url: Faker::Internet.url,
+                  })
+  post.save
+end
 # TODO: Write a seed to insert 100 posts in the database
