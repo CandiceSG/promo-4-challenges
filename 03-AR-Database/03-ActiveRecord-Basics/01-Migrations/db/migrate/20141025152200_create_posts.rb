7bf4859091b12fe2f0215c07c94d6746e31e1f9e
CandiceSG <candice.salagasperini@gmail.com>
2014-10-27 12:11:33 +0100

    exo 1

--- 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/db/migrate/20141025152200_create_posts.rb
@@ -1,5 +1,23 @@
 class CreatePosts < ActiveRecord::Migration
   def change
-    # TODO: your code here to create the posts table
+    create_table :posts do |t|
+        t.string :name
+        t.string :url
+        t.timestamps
+    end
   end
 end
+
+class AddUrlToCreatePosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :url, :string
+  end
+end
+
+class AddVotesToCreatePosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :votes, :integer
+  end
+end
+
+# TODO: your code here to create the posts table
