122b12b06ec05da05e546e9580d2a67b83aeb718
CandiceSG <candice.salagasperini@gmail.com>
2014-10-27 18:09:03 +0100

    exo 4

--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/db/seeds.rb
@@ -1,4 +1,15 @@
 require "json"
 require "rest-client"
 
+
+response = RestClient.get "https://hacker-news.firebaseio.com/v0/topstories.json"
+repos = JSON.parse(response).take(10)
+
+repos.each do |id|
+url = "https://hacker-news.firebaseio.com/v0/item/#{id}.json"
+  Json_post = RestClient.get(url)
+  post = JSON.parse(Json_post)
+  Post.create(name: post["title"], url: post["url"], votes: post["score"])
+end
+
 # TODO: Write a seed to insert 100 posts in the database fetched from the Hacker News API.
