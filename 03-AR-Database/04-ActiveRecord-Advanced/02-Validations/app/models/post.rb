d3244b5f1a888f5369c0d2c481b4ca7047fdec87
CandiceSG <candice.salagasperini@gmail.com>
2014-10-28 12:19:10 +0100

    ok

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -2,8 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :name
   validates :name, length: { minimum: 5 }
-  validates :name, uniqueness: {case_sensitive: false}
-  validates_presence_of :url, format:{ }
+  validates :name, uniqueness: { case_sensitive: false }
+  validates_presence_of :url, format: {}
   validates_presence_of :user
-# TODO: Add some validation
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_many :posts
   validates_presence_of :username
   validates_uniqueness_of :username
-  validates_presence_of :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i}
+  validates_presence_of :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
 end
