d37d9f174c81dd60a2e2dffaf54f20b77b0d06ac
CandiceSG <candice.salagasperini@gmail.com>
2014-10-28 11:25:25 +0100

    exo 1

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,15 +1,15 @@
 require "faker"
 
 5.times do
-  user = User.create( {
+  user = User.create({
     :username   => Faker::Internet.user_name,
     :email       => Faker::Internet.email
-                 })
+                     })
 
   (5..10).to_a.sample.times do
     user.posts.create(
       name: Faker::Lorem.word,
-      url: Faker::Internet.url,
+      url: Faker::Internet.url
     )
   end
 end
