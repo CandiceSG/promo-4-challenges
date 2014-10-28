bddd3dd7c153d0639698316c89eccc131ac05773
CandiceSG <candice.salagasperini@gmail.com>
2014-10-28 14:36:16 +0100

    ok

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
   has_many :posts
-  before_validation :stripspaces
+  before_validation :stripemail
   validates :username, :email, presence: true
   validates_uniqueness_of :username
   validates :email, format: { with: /\A\w+@\w+\.\w{2,3}\z/, message: 'invalid email' }
 
   private
 
-  def stripspaces
+  def stripemail
     self.email = email.lstrip.rstrip unless email.nil?
   end
 end
