071cc6bddfdea03a5e2b37cf8dcfa4bba5cedd4f
CandiceSG <candice.salagasperini@gmail.com>
2014-10-28 12:16:41 +0100

    ok

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,16 +1,17 @@
 require "faker"
 
 5.times do
-  user = User.create({
-    :username   => Faker::Internet.user_name,
-    :email       => Faker::Internet.email
-                     })
+  user = User.create(
+     username: Faker::Internet.user_name,
+     email: Faker::Internet.email
+
+  )
 
   (5..10).to_a.sample.times do
     user.posts.create(
       name: Faker::Lorem.word,
       url: Faker::Internet.url
-    )
+  )
   end
 end
 # TODO: Write a seed
