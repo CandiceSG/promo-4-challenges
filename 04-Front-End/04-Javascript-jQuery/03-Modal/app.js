24a0f4d453015be649afed455ec914981d0a4a0c
CandiceSG <candice.salagasperini@gmail.com>
2014-11-03 18:33:36 +0100

    exo 3 corrigé

--- 04-Front-End/04-Javascript-jQuery/03-Modal/app.js
+++ 04-Front-End/04-Javascript-jQuery/03-Modal/app.js
@@ -1,16 +1,28 @@
 $(function() {
-  var bg = $('<div>').addClass("background");
-  var modalDiv = $('<div>').addClass("image");
 
   $("img").on("click", function(e){
-    $('body').append(bg);
-    img = $("<img src='" +$(this).attr("src") + "'>)");
-    img.appendTo(modalDiv);
-    $('body').append(modalDiv);
+    // We define 2 html divs for modal and background
+    var bg = $('<div>').addClass("background");
+    var modal = $('<div>').addClass("modal");
+    modal.css("width", $(this).data("modal-width") + "px")
 
-    $(bg).on('click',function(e){
-      $(bg).remove();
-      $(modalDiv).remove();
-    })
+
+    // We append bg to body
+    $('body').append(bg);
+
+    // We build img, fix its width & insert it into our modal
+    img = $("<img src='" + $(this).attr("src") + "'>)");
+    img.appendTo(modal);
+    var modalWrapper = $('<div>').addClass("modal-wrapper");
+    modal.appendTo(modalWrapper)
+
+    // Finally we append our nice modal to the body
+    $('body').append(modalWrapper);
+
+      modalWrapper.on("click", function(e){
+      modalWrapper.remove();
+      modal.remove();
+      bg.remove();
+    });
   });
 });
