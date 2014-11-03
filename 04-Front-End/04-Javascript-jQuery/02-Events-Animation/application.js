29e6edf35ce8506857656f3cb9e4a7279b1304a2
CandiceSG <candice.salagasperini@gmail.com>
2014-11-03 14:40:17 +0100

    exo 2

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -1,3 +1,16 @@
+$(function() {
+  $("#message").hide().delay(500).slideDown('slow');
+
+  $( "#message" ).on('click', function(e){
+    $(this).slideUp( "slow")
+  });
+
+  $("#message-wrapper").on('mouseenter', function(e){
+    $("#message").slideDown('slow')
+  });
+
+});
+
 /*
   *
   * TODO: add your code here!
